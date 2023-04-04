61349753d7a235729d2f37b44fed0316c264fd8e
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-04 14:36:00 +0300

    Create a simple CLI calculator using Commander package

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+const { program } = require("commander");
 const readline = require("readline");
-const colors = require("colors");
+const colors = require("colors"); // used for changing text colors in the terminal
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+program.version("1.0.0").description("Simple CLI calculator"); // if user types 'node index.js' then he will see the instructions how to use it.
 
-rl.question("Enter your expression: ".red, (expression) => {
-  const result = eval(expression);
-  console.log(`${result}`.red);
-  rl.close();
-});
+program
+  .command("calc <expression>") // command to calculate
+  .description("Calculates math expression") // description of the cli app
+  .action((expression) => {
+    const result = eval(expression);
+    console.log(`Result is: ${result}`.red);
+  });
+
+program.parse(process.argv); // launch the app
