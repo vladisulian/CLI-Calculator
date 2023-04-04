686d0d62c9f7a3f76c08e863546c595ba8936e5f
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-04 14:49:58 +0300

    Add --help example in index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const { program } = require("commander");
 const readline = require("readline");
 const colors = require("colors"); // used for changing text colors in the terminal
 
-program.version("1.0.0").description("Simple CLI calculator"); // if user types 'node index.js' then he will see the instructions how to use it.
+program.version("1.0.0").description("Simple CLI calculator".green); // if user types 'node index.js' then he will see the instructions how to use it.
 
 program
-  .command("calc <expression>") // command to calculate
-  .description("Calculates math expression") // description of the cli app
+  .command("calc <expression>".green) // command to calculate
+  .description(" Calculates math expression. Example: node index.js calc '2 + 2'".green) // description of the cli app
   .action((expression) => {
     const result = eval(expression);
     console.log(`Result is: ${result}`.red);
