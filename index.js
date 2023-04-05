88987a917385e91dc38999127b8e5b68df8eadf9
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-06 01:33:51 +0300

    Fix bug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const colors = require("colors"); // used for changing text colors in the termin
 program.version("1.0.0").description("Simple CLI calculator".green); // if user types 'node index.js' then he will see the instructions how to use it.
 
 program
-  .command("calc <expression>".green) // command to calculate
+  .command("calc <expression>") // command to calculate
   .description(" Calculates math expression. Example: node index.js calc '2 + 2'".green) // description of the cli app
   .action((expression) => {
     const result = eval(expression);
